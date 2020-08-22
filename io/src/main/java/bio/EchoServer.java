d790779fe19eb01be4eebec639e458244fb2786d
Shoukai Huang <huangshoukai@renrenche.com>
2020-08-22 23:31:39 +0800

    bio

---
+++ io/src/main/java/bio/EchoServer.java
@@ -0,0 +1,36 @@
+package bio;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class EchoServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(8080);
+        while (true) {
+            System.out.println("start accept");
+            Socket socket = serverSocket.accept();
+            System.out.println("new conn: " + socket.getRemoteSocketAddress());
+
+            new Thread(() -> {
+                try {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    String msg;
+                    while ((msg = reader.readLine()) != null) {
+                        if (msg.equalsIgnoreCase("quit")) {
+                            reader.close();
+                            socket.close();
+                            break;
+                        } else {
+                            System.out.println("receive msg: " + msg);
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        }
+    }
+}
